f6ed1a5c8777918a05536242ebd92ac9154e6189
Gopika B.S <letters.to.gopika@gmail.com>
2023-09-06 19:26:52 +0530

    Static webpage using react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import React from "react";
+import Navbar from "./components/Navbar.jsx";
+import MainContent from "./components/MainContent.jsx";
+import "./App.css";
+function App() {
+  return (
+    <div className="container">
+      <Navbar />
+      <MainContent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+// import "./App.css";
+
+function Navbar() {
+  return (
+    <nav className="nav">
+      <div className="nav-1">
+        <img
+          src="./public/images/react-icon-small.png"
+          className="navlogo"
+          alt="react-logo"
+        />
+        <h2>ReactFacts</h2>
+      </div>
+      <div className="nav-2">
+        <h3>React Course - Project 1</h3>
+      </div>
+    </nav>
+  );
+}
+export default Navbar;
